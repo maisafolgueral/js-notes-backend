bdeb5fbcbd95844855d8f4a924472365971c927d
Maisa <maisa.folgueral@gmail.com>
2023-06-01 14:07:36 -0300

    Added API base

---
+++ config/database.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect("mongodb://localhost/javascriptNote", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+  })
+  .then(() => console.log("Connection succesful"))
+  .catch((err) => console.error(err));
