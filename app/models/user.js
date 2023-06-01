cad133d5e16eadb827cd1abf2132a21dd965862e
Maisa <maisa.folgueral@gmail.com>
2023-06-01 17:22:36 -0300

    Created data structure

---
+++ app/models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", userSchema);
