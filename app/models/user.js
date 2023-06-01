d3b8893515a5c5ad91e7c811f789bd8e8880465c
Maisa <maisa.folgueral@gmail.com>
2023-06-01 18:15:25 -0300

    Added hash password

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,26 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-var userSchema = new mongoose.Schema({
+let userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
+});
+
+userSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("password")) {
+    bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+      if (err) next(err);
+      else {
+        this.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
