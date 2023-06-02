c672b215365dc1fb1ef038feeed1dbedd61e3d54
Maisa <maisa.folgueral@gmail.com>
2023-06-02 17:04:38 -0300

    Added update note

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -35,6 +35,25 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
+router.put("/:id", withAuth, async (req, res) => {
+  const { title, body } = req.body;
+  const { id } = req.params;
+
+  try {
+    let note = await Note.findById(id);
+    if (isOwner(req.user, note)) {
+      let note = await Note.findOneAndUpdate(
+        {_id: id},
+        { $set: { title: title, body: body}},
+        { upsert: true, 'new': true }
+      )
+      res.json(note);
+    } else res.status(403).json({ error: "Permission denied" });
+  } catch (err) {
+    res.status(500).send({ error: "Problem to update a note." });
+  }
+});
+
 const isOwner = (user, note) => {
   if (JSON.stringify(user._id) == JSON.stringify(note.author._id)) return true;
   else return false;
